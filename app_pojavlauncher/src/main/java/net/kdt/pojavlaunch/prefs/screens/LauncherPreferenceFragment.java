f339de6ad56915e563a7c99b77c9ee5dca901bd1
azizemirmercan-hub <azizemirmercan@gmail.com>
2026-05-01 20:00:11 +0300

    Refactor LauncherPreferenceFragment and remove unused code

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceFragment.java
@@ -1,81 +1,59 @@
 package net.kdt.pojavlaunch.prefs.screens;
 
-
-import android.app.Activity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-
-import net.kdt.pojavlaunch.LauncherActivity;
+import androidx.preference.PreferenceGroup;
 import git.artdeell.mojo.R;
 import net.kdt.pojavlaunch.prefs.LauncherPreferences;
 
 /**
- * Preference for the main screen, any sub-screen should inherit this class for consistent behavior,
- * overriding only onCreatePreferences
+ * Moonlight OS mantığıyla tüm GUI kısıtlamalarını kaldıran Full Mode Fragment.
  */
-public class LauncherPreferenceFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class LauncherPreferenceFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // Arka plan rengini ayarla
         view.setBackgroundColor(getResources().getColor(R.color.background_app));
         super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
     public void onCreatePreferences(Bundle b, String str) {
+        // Ana ayar dosyasını yükle
         addPreferencesFromResource(R.xml.pref_main);
-        setupNotificationRequestPreference();
+        
+        // KRİTİK: Tüm kilitleri açan metod
+        unlockEverything(getPreferenceScreen());
     }
 
-    private void setupNotificationRequestPreference() {
-        Preference mRequestNotificationPermissionPreference = requirePreference("notification_permission_request");
-        Activity activity = getActivity();
-        if(activity instanceof LauncherActivity) {
-            LauncherActivity launcherActivity = (LauncherActivity)activity;
-            mRequestNotificationPermissionPreference.setVisible(!launcherActivity.checkForNotificationPermission());
-            mRequestNotificationPermissionPreference.setOnPreferenceClickListener(preference -> {
-                launcherActivity.askForNotificationPermission(()->mRequestNotificationPermissionPreference.setVisible(false));
-                return true;
-            });
-        }else{
-            mRequestNotificationPermissionPreference.setVisible(false);
+    /**
+     * Bu metod, XML içinde gizlenmiş (visible=false) veya 
+     * tıklanamaz (enabled=false) yapılmış her şeyi "ZORLA" aktif eder.
+     */
+    private void unlockEverything(PreferenceGroup group) {
+        for (int i = 0; i < group.getPreferenceCount(); i++) {
+            Preference p = group.getPreference(i);
+            
+            // Ayarı görünür ve tıklanabilir yap
+            p.setVisible(true);
+            p.setEnabled(true);
+
+            // Eğer bu bir alt menü (kategori) ise, onun içine de gir ve her şeyi aç
+            if (p instanceof PreferenceGroup) {
+                unlockEverything((PreferenceGroup) p);
+            }
         }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
-        if(sharedPreferences != null) sharedPreferences.registerOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    public void onPause() {
-        SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
-        if(sharedPreferences != null) sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
-        super.onPause();
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences p, String s) {
+        // Tercihleri her seferinde yeniden yükle
         LauncherPreferences.loadPreferences(getContext());
-    }
-
-    protected Preference requirePreference(CharSequence key) {
-        Preference preference = findPreference(key);
-        if(preference != null) return preference;
-        throw new IllegalStateException("Preference "+key+" is null");
-    }
-    @SuppressWarnings("unchecked")
-    protected <T extends Preference> T requirePreference(CharSequence key, Class<T> preferenceClass) {
-        Preference preference = requirePreference(key);
-        if(preferenceClass.isInstance(preference)) return (T)preference;
-        throw new IllegalStateException("Preference "+key+" is not an instance of "+preferenceClass.getSimpleName());
     }
 }
